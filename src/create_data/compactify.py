dd9a013083fcbf4435684bd007abace77dd813d1
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-20 20:51:41 +0400

    compactify works with out_dir now

--- src/create_data/compactify.py
+++ src/create_data/compactify.py
@@ -1,15 +1,21 @@
-from cPickle import load, dump
+import cPickle
 from scipy.io import savemat, mmwrite
 from scipy.sparse import coo_matrix
 
-def create_compact_dicts():
+def create_compact_dicts(out_dir, in_dir):
     print 'compactifying...'
     sparse_to_dense, dense_to_sparse = {}, {}
+    f = open(in_dir + 'graph.txt')
+    lines = f.readlines()
+    f.close()
+    
+    mk_step = 20
+    step = len(lines) / mk_step
     k = 0
-    for line in open('graph.txt'):
+    for line in lines:
         k = k + 1
-        if k % 100000 == 0:
-            print k
+        if k % step == 0:
+            print "%d%% done" % (100 / mk_step * k / step)
         # FIX id generation!!!1111
         IDs = line.split()
         for id in IDs:
@@ -18,16 +24,17 @@ def create_compact_dicts():
                 i = len(dense_to_sparse)
                 sparse_to_dense[ID] = i
                 dense_to_sparse[i] = ID
-    #print len(sparse_to_dense)
-    #print len(dense_to_sparse)
-    dump(sparse_to_dense, open('sparse_to_dense.pickle', 'w'), 2)
-    dump(dense_to_sparse, open('dense_to_sparse.pickle', 'w'), 2)
-
-def create_matrix():
-    sparse_to_dense = load(open('sparse_to_dense.pickle'))
+    cPickle.dump(sparse_to_dense, open(out_dir + 'sparse_to_dense.pickle', 
+                                       'w'), 2)
+    cPickle.dump(dense_to_sparse, open(out_dir + 'dense_to_sparse.pickle', 
+                                       'w'), 2)
+    print 'compactifying... Done.'
+    
+def create_matrix(in_dir):
+    sparse_to_dense = cPickle.load(open(in_dir + 'sparse_to_dense.pickle'))
     print 'reading graph file and matrixifying...'
     I, J = [], []
-    for line in open('graph.txt'):
+    for line in open(in_dir + 'graph.txt'):
         converted = [sparse_to_dense.get(int(ID)) for ID in line.split()]
         i = converted[0]
         j = converted[1]
@@ -35,12 +42,16 @@ def create_matrix():
         J.append(j)
     n = max([max(I), max(J)]) + 1
     data = [1]*len(I)
+    print 'reading graph file and matrixifying... Done.'
+    
     return coo_matrix((data, (I,J)), shape=(n,n), dtype='i4')
 
-def main():
-    create_compact_dicts()
-    W = create_matrix()
-    savemat('W.mat', dict(W=W))
+def main(out_dir, in_dir = '../../data/'):
+    create_compact_dicts(out_dir, in_dir)
+    W = create_matrix(in_dir)
+    savemat(out_dir + 'W.mat', dict(W=W), oned_as='column')
+    # rdkl:
+    # Maybe here should be: savemat('W.mat', dict(W=W), oned_as='column')
 
 if __name__ == '__main__':
     main()

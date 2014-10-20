b1d534cdcd771d384749837abd44127540994f8c
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-20 20:39:43 +0400

    make_graph works with out_dir now

--- src/create_data/make_graph.py
+++ src/create_data/make_graph.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-from re import finditer
-from cPickle import load
+
+import cPickle
+import re
 
 def process_line(line, t2i, i2t, outfile):
     ''' 
@@ -12,39 +13,42 @@ def process_line(line, t2i, i2t, outfile):
     Each tuple is of the form ('from' page, namespace, 'to' page).  Print a
     line for each 'from' page with the outlinks that are in namespace 0 (the
     main wikipedia, ignores 'talk' pages, etc). Annoyingly, the 'from' pages
-    are given by ID and the 'to' pages by name. Use the dictionary d to map the
-    text names to IDs for consistency (and some space savings).  
+    are given by ID and the 'to' pages by name. Use the dictionary d to map 
+    the text names to IDs for consistency (and some space savings).  
     '''
+    
     pattern = "\((\d+),(\d+),'(.*?)',(\d+)\)[,;]"
-    current_page = None
-    for match in finditer(pattern, line):
-        from_page, namespace, to_page, namespce2 = match.groups()
+    for match in re.finditer(pattern, line):
+        from_page, namespace, to_page, _ = match.groups()
         if not to_page in t2i:
             continue
+        
         if namespace != '0':
-            #print line
             continue
-        #print from_page, to_page
+
         if not int(from_page) in i2t:
             continue
+        
         outfile.write(from_page + ' ')
         outfile.write(str(t2i[to_page]) + ' ')
         outfile.write('\n') 
 
-def main():
+def main(out_dir, in_dir = '../../data/'):
     ''' Reads pagelinks.sql line by line and processes it. Needs the pickled 
     dictionary mapping page names to IDs '''
     print "building the graph..."
     crap = 'INSERT INTO `pagelinks` VALUES'
-    path = ''#set if needed (different current dir)
-    pickle = 'title-ID_dict.pickle'
-    t2i = load(open(path+pickle))
-    i2t = load(open(path+'ID-title_dict.pickle'))
+    
+    file_name = 'title-ID_dict.pickle'
+    t2i = cPickle.load(open(in_dir + file_name))
+    i2t = cPickle.load(open(in_dir + 'ID-title_dict.pickle'))
 
-    with open('graph.txt', 'w') as outfile:
-        for line in open('pagelinks.sql'):
+    with open(out_dir + 'graph.txt', 'w') as outfile:
+        for line in open(in_dir + 'pagelinks.sql'):
             if line[:len(crap)] == crap: 
                 process_line(line, t2i, i2t, outfile)
+
+    print "building the graph... Done."
 
 if __name__ == "__main__":
     main()

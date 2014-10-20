00f291875dc8f97c94ee4482bdbf32c9f01a16f1
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-20 20:52:40 +0400

    download_and_extract works with out_dir now

--- src/create_data/download_and_extract.sh
+++ src/create_data/download_and_extract.sh
@@ -1,6 +1,7 @@
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz -P ../data/
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../data/
-gunzip -c ../data/*page.sql.gz > ../data/page.sql
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz -P ../data/
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../data/ 
-gunzip -c ../data/*pagelinks.sql.gz > ../data/pagelinks.sql
+wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz -P ../../data/
+#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../../data/
+gunzip -c ../../data/*page.sql.gz > ../../data/page.sql
+
+wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz -P ../../data/
+#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../../data/ 
+gunzip -c ../../data/*pagelinks.sql.gz > ../../data/pagelinks.sql

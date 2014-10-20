b808bbfec070b3a37b958d6b1c1041fe5f30f438
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-20 19:51:01 +0400

    download script data -> /data/

--- src/download_and_extract.sh
+++ src/download_and_extract.sh
@@ -1,6 +1,6 @@
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz
-gunzip -c *page.sql.gz > page.sql
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz
-gunzip -c *pagelinks.sql.gz > pagelinks.sql
+wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz -P ../data/
+#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../data/
+gunzip -c ../data/*page.sql.gz > ../data/page.sql
+wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz -P ../data/
+#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../data/ 
+gunzip -c ../data/*pagelinks.sql.gz > ../data/pagelinks.sql

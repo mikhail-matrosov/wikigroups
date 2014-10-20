d8b40cd882c1097e8687c3f0e938aa979ec6bfd8
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-20 20:32:28 +0400

    title <-> ID works with out_dir now

--- src/create_data/make_title_ID_dicts.py
+++ src/create_data/make_title_ID_dicts.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python
-from re import finditer
-from cPickle import dump
+import cPickle
+import re
 
 def process_line(line, d):
     ''' gets the ID and name for each page in the line '''
     pattern = "\((\d+),(\d+),'(.*?)','"
-    for match in finditer(pattern, line):
+    for match in re.finditer(pattern, line):
         ID, namespace, name = match.groups()
         if namespace == '0': 
             d[name] = int(ID)
 
-def main(infile = 'page.sql'):
+def main(out_dir, in_dir = '../../data/', infile = 'page.sql'):
     ''' Reads page.sql line by line and processes it '''
     print 'making title <--> ID dictionaries...'
     crap = 'INSERT INTO `page` VALUES'
     t2id = {}
-    for line in open(infile):
+    for line in open(in_dir + infile):
         if line[:len(crap)] == crap: 
             process_line(line, t2id)
     id2t = {v:k for k, v in t2id.iteritems()}
-    dump(t2id, open('title-ID_dict.pickle', 'w'), 2)
-    dump(id2t, open('ID-title_dict.pickle', 'w'), 2)
+    cPickle.dump(t2id, open(out_dir + 'title-ID_dict.pickle', 'w'), 2)
+    cPickle.dump(id2t, open(out_dir + 'ID-title_dict.pickle', 'w'), 2)
+    print 'making title <--> ID dictionaries... Done.'
 
 if __name__ == "__main__":
     main()

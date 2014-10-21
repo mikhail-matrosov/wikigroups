406c8465a9957e1f054ec7e0582ab16c042783b9
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-10-21 16:07:59 +0400

    Fix code style

--- src/alg/KMeans.py
+++ src/alg/KMeans.py
@@ -11,30 +11,24 @@ def KMeans(A, args):
     Output:
     cluster - list of the row index of the vertices from the cluster
     '''
-    
     N = A.shape[0]
-    K = args['number_of_clusters'] # it is K-means, isn't it?
+    K = args['number_of_clusters']  # it is K-means, isn't it?
     N_ITERATIONS = args['max_number_of_iterations']
-    
     # cluster_ids[0,:] = 0, its a workaround for argmax
-    clusters_membership = np.zeros((K+1, N), np.int32)
-    
+    clusters_membership = np.zeros((K + 1, N), np.int32)
     # make an initial guess - K different random indexes
     initial_centers = np.random.choice(N, K, replace=False)
-    for i,k in enumerate(initial_centers):
-        clusters_membership[i,k+1] = 1
-    
+    for i, k in enumerate(initial_centers):
+        clusters_membership[i, k + 1] = 1
     # iterate until converge
     for iteration in xrange(N_ITERATIONS):
-        print 'iteration #%d' % (iteration+1,)
+        print 'iteration #%d' % (iteration + 1,)
         dists = A.dot(clusters_membership.T)
-        ix = dists.argmax(1) # ids where to assign each vertex
-        nz = np.where(ix)[0] # non-zero indexes
-        
-        clusters_membership[:,nz] = 0
+        ix = dists.argmax(1)  # ids where to assign each vertex
+        nz = np.where(ix)[0]  # non-zero indexes
+        clusters_membership[:, nz] = 0
         for i in nz:
             clusters_membership[ix[i], i] = 1
-        
     # organize return
-    clusters = [np.where(clusters_membership[k+1,:])[0] for k in range(K)]
+    clusters = [np.where(clusters_membership[k + 1, :])[0] for k in range(K)]
     return clusters

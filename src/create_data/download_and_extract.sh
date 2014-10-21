95d7add6029d08497bada43e0dd5e1cef7c8fa1a
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-10-21 23:11:06 +0400

    Add download and unzip the dump with category links

--- src/create_data/download_and_extract.sh
+++ src/create_data/download_and_extract.sh
@@ -1,7 +1,10 @@
 wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz -P ../../data/
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../../data/
+# wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../../data/
 gunzip -c ../../data/*page.sql.gz > ../../data/page.sql
 
 wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz -P ../../data/
-#wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../../data/ 
+# wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../../data/ 
 gunzip -c ../../data/*pagelinks.sql.gz > ../../data/pagelinks.sql
+
+wget https://dumps.wikimedia.org/simplewiki/20140814/simplewiki-20140814-categorylinks.sql.gz -P ../../data/
+gunzip -c ../../data/simplewiki-20140814-categorylinks.sql.gz > ../../data/categorylinks.sql

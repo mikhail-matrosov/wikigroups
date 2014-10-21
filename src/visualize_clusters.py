a3b03ed36014d5d0e9ad9c3e94d2b6494621fa5a
mikhail-matrosov <ktdfly@gmail.com>
2014-10-21 19:57:45 +0400

    Visualizator's fix

--- src/visualize_clusters.py
+++ src/visualize_clusters.py
@@ -9,6 +9,9 @@ def visualizeClusters(A, clusters):
     clusters - list of the row index of the vertices from the cluster
     '''
     
+    size_treshold = 1e-4 * max([len(c) for c in clusters])
+    clusters = [c for c in clusters if len(c)>size_treshold]
+    
     K = len(clusters) # number of clusters
     B = np.zeros((K,K)) # clusters' network
     
@@ -16,32 +19,37 @@ def visualizeClusters(A, clusters):
         for j in range(K):
             B[i,j] = A[clusters[i], :][:,clusters[j]].sum()
     
+    B = np.log(B+1)
+    
     G = nx.DiGraph(B);
     #nx.draw_spectral(G);
     
     # make new undirected graph H without multi-edges
     H=nx.Graph(G)
 
-    # edge width is proportional number of games played
+    # edge width is proportional to similiarity of nodees
     edgewidth=[]
     for (u,v,d) in H.edges(data=True):
-        edgewidth.append(G.get_edge_data(u,v)['weight'])
+        edge = G.get_edge_data(u,v)
+        if edge:
+            edgewidth.append(edge['weight'])
 
-    # node size is proportional to number of games won
-    sizes = dict(zip(G.nodes(), map(len, clusters)))
+    # node size is proportional to number of articles
+    sizes = dict(zip(G.nodes(), map(lambda x: np.sqrt(len(x)), clusters)))
     
     labels = dict(zip(G.nodes(), map(len, clusters)))
     
-    pos=nx.spectral_layout(H)
-        
+    #pos = nx.spectral_layout(H)
+    pos = nx.spring_layout(H)
+    
     plt.rcParams['text.usetex'] = False
     plt.figure(figsize=(8,8))
-    nx.draw_networkx_edges(H,pos,alpha=0.5,width=edgewidth, edge_color='k')
+    nx.draw_networkx_edges(H,pos,alpha=0.4,width=edgewidth, edge_color='k')
     nodesize=[sizes[v]*50 for v in H]
     nx.draw_networkx_nodes(H,pos,node_size=nodesize,node_color='r',alpha=0.8)
     nx.draw_networkx_edges(H,pos,alpha=1,node_size=0,width=1,edge_color='k')
     nx.draw_networkx_labels(H,pos,labels,fontsize=14)
 
     plt.axis('off')
-    #plt.savefig("chess_masters.png",dpi=75)
+    #plt.savefig("graph.png",dpi=75)
     plt.show() # display

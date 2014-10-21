6bc5b985ae60c1f93e09d2d705f6a43a14ad8a8b
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-10-21 23:21:45 +0400

    Update category dump

--- src/create_data/download_and_extract.sh
+++ src/create_data/download_and_extract.sh
@@ -6,5 +6,6 @@ wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sq
 # wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../../data/ 
 gunzip -c ../../data/*pagelinks.sql.gz > ../../data/pagelinks.sql
 
-wget https://dumps.wikimedia.org/simplewiki/20140814/simplewiki-20140814-categorylinks.sql.gz -P ../../data/
-gunzip -c ../../data/simplewiki-20140814-categorylinks.sql.gz > ../../data/categorylinks.sql
+# wget https://dumps.wikimedia.org/simplewiki/20140814/simplewiki-20140814-categorylinks.sql.gz -P ../../data/
+wget https://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-categorylinks.sql.gz -P ../../data/
+gunzip -c ../../data/*categorylinks.sql.gz > ../../data/categorylinks.sql

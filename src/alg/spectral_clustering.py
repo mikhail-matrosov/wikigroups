8ad77d87087477448a4b274c11611e6ab83d3b48
Alexandr Katrutsa <amkatrutsa@yandex.ru>
2014-10-23 03:38:59 +0400

    Add test procedure

--- src/alg/spectral_clustering.py
+++ src/alg/spectral_clustering.py
@@ -14,24 +14,28 @@ def spectral_clustering(A, args):
     to cluster with some cluster algorithm 
     '''
     if type(A) == np.ndarray:
+        print 'Building graph...'
         G = nx.from_numpy_matrix(A)
+        laplacian = nx.laplacian_matrix(G)
     elif type(A) == scsp.csc.csc_matrix:
+        print 'Building graph...'
         G = nx.from_scipy_sparse_matrix(A)
-    if args['show_eigenvalues']:
-        laplacian_eigval = nx.laplacian_spectrum(G)
-        plt.scatter(np.array(range(len(laplacian_eigval))) + 1, 
-                             laplacian_eigval, marker='x', s=90)
-        plt.show()
-    laplacian = nx.laplacian_matrix(G)
+        laplacian = nx.laplacian_matrix(G)
+    print 'Start finding reduction...'
     max_eigvals = scspl.eigs(laplacian * 1.0, return_eigenvectors=False, 
-                             k=laplacian.shape[0] - 2)
+                             k=args['dim'] + 10)
     alpha = np.amax(max_eigvals)
     modif_laplacian = alpha * scsp.eye(laplacian.shape[0], 
                                        laplacian.shape[1]) - laplacian
     lap_eigval, lap_eigvec = scspl.eigs(modif_laplacian * 1.0, 
-                                        k=args['dim'])
-    U = lap_eigvec.real
-    if args['dim'] < 1000:
+                                        k=20)
+    if args['show_eigenvalues']:
+        print 'Show eigenvalues...'
+        plt.scatter(np.array(range(len(lap_eigval))) + 1, np.sort(-lap_eigval.real + alpha), marker='x', s=90)
+        plt.show()
+    U = lap_eigvec[:, 0:args['dim']].real
+    print 'Start clustering...'
+    if U.shape[0] < 1000:
         clustering = skcl.KMeans(n_clusters=args['number_of_clusters'])
         clustering.fit(U)
     else:
@@ -43,7 +47,12 @@ def spectral_clustering(A, args):
 if __name__ == '__main__':
     print "Testing..."
     test_matrix = scipy.io.loadmat('../data/test/W.mat')['W']
-    input_matrix = scipy.io.loadmat('../data/A.mat')['A']
-    args = {'dim': 2, 'number_of_clusters': 2, 'show_eigenvalues': True}
+    # input_matrix = scipy.io.loadmat('../data/A.mat')['A']
+    args = {'dim': 3, 'number_of_clusters': 3, 'show_eigenvalues': False}
     cl_test = spectral_clustering(test_matrix, args)
-#    cl_real = spectral_clustering(input_matrix, args)
+    # cl_real = spectral_clustering(input_matrix, args)
+    with open('../../data/cluster_id.txt' , 'w') as f:
+        for i in cl_test:
+            f.write(str(i) + '\n')
+            
+    print 'Testing... Done.'

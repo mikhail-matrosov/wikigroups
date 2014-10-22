4c6101a6b4ae128acd99fb41b337d34e2000adba
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-22 18:06:32 +0400

    Files downloading fixed

--- src/create_data/download_and_extract.sh
+++ src/create_data/download_and_extract.sh
@@ -1,11 +1,8 @@
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz -P ../../data/
-# wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-page.sql.gz -P ../../data/
+curl http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-page.sql.gz > ../../data/simplewiki-latest-page.sql.gz --progress-bar
 gunzip -c ../../data/*page.sql.gz > ../../data/page.sql
 
-wget http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz -P ../../data/
-# wget -c http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pagelinks.sql.gz -P ../../data/ 
+curl http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-pagelinks.sql.gz > ../../data/simplewiki-latest-pagelinks.sql.gz --progress-bar
 gunzip -c ../../data/*pagelinks.sql.gz > ../../data/pagelinks.sql
 
-# wget https://dumps.wikimedia.org/simplewiki/20140814/simplewiki-20140814-categorylinks.sql.gz -P ../../data/
-wget https://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-categorylinks.sql.gz -P ../../data/
+curl http://dumps.wikimedia.org/simplewiki/latest/simplewiki-latest-categorylinks.sql.gz > ../../data/simplewiki-latest-categorylinks.sql.gz --progress-bar
 gunzip -c ../../data/*categorylinks.sql.gz > ../../data/categorylinks.sql

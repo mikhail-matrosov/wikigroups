1071ad3a51b939c62f3731c5c4e029685869d855
Sergey Voronov <rdkl.hrd@gmail.com>
2014-10-24 01:17:57 +0400

    Improved perfomance (x10) and rewrited

--- src/alg/min_sum_distance.py
+++ src/alg/min_sum_distance.py
@@ -1,3 +1,6 @@
+import collections
+import math
+
 import numpy as np
 
 
@@ -6,22 +9,43 @@ def min_sum_distance(A, args):
     Function extracts cluster based on the minimum sum of distances
     from current vertex to all vertices in cluster. If the sum of distances
     from the vertex is minimum and the cardinality of cluster is less than
-    given value, then this vertex is added to the custer.
+    given value, then this vertex is added to the cluster.
     Input:
     A - adjacent matrix of wiki-graph
     Output:
-    cluster - list of the row index of the vertices fron the cluster
+    cluster - list of the row index of the vertices from the cluster
     '''
     S = args['matrix_pairwise_sim'].tocsr()
-    cluster = [args['index']]
-    while (len(cluster) < args['size']):
-        print 'Current size of cluster', len(cluster)
-        current_distances = [0 for i in xrange(A.shape[0])]
-        for idx_vertex in xrange(A.shape[0]):
-            if idx_vertex not in cluster:
-                s = [0 for i in xrange(A.shape[0])]
-                for idx_vertex_in_cluster in cluster:
-                    s[idx_vertex_in_cluster] = S[idx_vertex, idx_vertex_in_cluster]
-                current_distances[idx_vertex] = np.sum(s)
-        cluster.append(np.argmax(current_distances))
-    return cluster
+    start_index = args['index']
+    cluster = collections.Counter()
+    cluster[start_index] = 1 
+    last_similarity = 0
+    
+    while (True):
+        print 'Current size of cluster:', len(cluster)
+        if (len(cluster) > args['size']):
+            break
+        
+        similarities = np.zeros(A.shape[0])
+        for vertex in xrange(A.shape[0]):
+            if vertex not in cluster:
+                for cluster_vertex in cluster:
+                    if cluster_vertex != start_index:
+                        similarities[vertex] += S[vertex, cluster_vertex]
+                    else:
+                        similarities[vertex] += S[vertex, cluster_vertex] * \
+                                    min(math.log(len(cluster) + 1, 2), 6)
+        
+        max_similarity = np.max(similarities)
+        max_sim_index = np.argmax(similarities)
+        
+        print 'Similarity:', max_similarity / (len(cluster) + 1), '\n'
+        if len(cluster) > 0 and \
+            (1 + 1.0 / math.log(len(cluster) + 1, 2)) * max_similarity < \
+             (1 + 1.0 / len(cluster)) * last_similarity:
+            print 'Cluster found. Algorithm was stopped.'
+            break
+        last_similarity = max_similarity                       
+        cluster[max_sim_index] = 1
+        #print cluster
+    return cluster.keys()
